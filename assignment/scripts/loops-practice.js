88a4d554196d5b372e5154cc8672a73ad24dcc1e
Dylan Brown <dylanbrown588@gmail.com>
2023-11-21 17:22:57 -0600

    count down from 5 to 0

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -29,7 +29,9 @@ console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
